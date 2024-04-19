4a8d95b85c7b40a02709568c2e8dafc568e005da
Estanley Tagalog <estanleyjt@gmail.com>
2024-04-19 21:57:19 +0800

    changed

--- admin/register.php
+++ admin/register.php
@@ -26,7 +26,7 @@ include('includes/navbar.php');
                 <input type="email" name="email" class="form-control" placeholder="Enter Email">
             </div>
             <div class="form-group">
-                <label>Password</label>
+                <label>Password</label> 
                 <input type="password" name="password" class="form-control" placeholder="Enter Password">
             </div>
             <div class="form-group">
@@ -83,13 +83,13 @@ include('includes/navbar.php');
             <td>
                 <form action="" method="post">
                     <input type="hidden" name="edit_id" value="">
-                    <button  type="submit" name="edit_btn" class="btn btn-success"> EDIT</button>
+                    <button  type="submit" name="edit_btn" class="btn btn-success"> Approved</button>
                 </form>
             </td>
             <td>
                 <form action="" method="post">
                   <input type="hidden" name="delete_id" value="">
-                  <button type="submit" name="delete_btn" class="btn btn-danger"> DELETE</button>
+                  <button type="submit" name="delete_btn" class="btn btn-danger"> Disapproved</button>
                 </form>
             </td>
           </tr>
