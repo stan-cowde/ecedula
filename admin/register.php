714c494a0626b72b89bd5230e188830cbc16c3bd
Roger Pantil <rogerpantilyowger@gmail.com>
2024-04-19 22:21:55 +0800

    modified login

--- admin/register.php
+++ admin/register.php
@@ -1,6 +1,6 @@
 <?php
-include('includes/header.php'); 
-include('includes/navbar.php'); 
+include('includes/header.php');
+include('includes/navbar.php');
 ?>
 
 
@@ -17,27 +17,27 @@ include('includes/navbar.php');
 
         <div class="modal-body">
 
-            <div class="form-group">
-                <label> Username </label>
-                <input type="text" name="username" class="form-control" placeholder="Enter Username">
-            </div>
-            <div class="form-group">
-                <label>Email</label>
-                <input type="email" name="email" class="form-control" placeholder="Enter Email">
-            </div>
-            <div class="form-group">
-                <label>Password</label>
-                <input type="password" name="password" class="form-control" placeholder="Enter Password">
-            </div>
-            <div class="form-group">
-                <label>Confirm Password</label>
-                <input type="password" name="confirmpassword" class="form-control" placeholder="Confirm Password">
-            </div>
-        
+          <div class="form-group">
+            <label> Username </label>
+            <input type="text" name="username" class="form-control" placeholder="Enter Username">
+          </div>
+          <div class="form-group">
+            <label>Email</label>
+            <input type="email" name="email" class="form-control" placeholder="Enter Email">
+          </div>
+          <div class="form-group">
+            <label>Password</label>
+            <input type="password" name="password" class="form-control" placeholder="Enter Password">
+          </div>
+          <div class="form-group">
+            <label>Confirm Password</label>
+            <input type="password" name="confirmpassword" class="form-control" placeholder="Confirm Password">
+          </div>
+
         </div>
         <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-            <button type="submit" name="registerbtn" class="btn btn-primary">Save</button>
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+          <button type="submit" name="registerbtn" class="btn btn-primary">Save</button>
         </div>
       </form>
 
@@ -48,58 +48,58 @@ include('includes/navbar.php');
 
 <div class="container-fluid">
 
-<!-- DataTales Example -->
-<div class="card shadow mb-4">
-  <div class="card-header py-3">
-    <h6 class="m-0 font-weight-bold text-primary">Admin Profile 
-            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#addadminprofile">
-              Add Admin Profile 
-            </button>
-    </h6>
-  </div>
+  <!-- DataTales Example -->
+  <div class="card shadow mb-4">
+    <div class="card-header py-3">
+      <h6 class="m-0 font-weight-bold text-primary">Admin Profile
+        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#addadminprofile">
+          Add Admin Profile
+        </button>
+      </h6>
+    </div>
 
-  <div class="card-body">
+    <div class="card-body">
 
-    <div class="table-responsive">
+      <div class="table-responsive">
 
-      <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
-        <thead>
-          <tr>
-            <th> ID </th>
-            <th> Username </th>
-            <th>Email </th>
-            <th>Password</th>
-            <th>EDIT </th>
-            <th>DELETE </th>
-          </tr>
-        </thead>
-        <tbody>
-     
-          <tr>
-            <td> 1 </td>
-            <td> peter</td>
-            <td> peter@gmail.com</td>
-            <td> *** </td>
-            <td>
+        <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+          <thead>
+            <tr>
+              <th> ID </th>
+              <th> Username </th>
+              <th>Email </th>
+              <th>Password</th>
+              <th>EDIT </th>
+              <th>DELETE </th>
+            </tr>
+          </thead>
+          <tbody>
+
+            <tr>
+              <td> 1 </td>
+              <td> peter</td>
+              <td> peter@gmail.com</td>
+              <td> *** </td>
+              <td>
                 <form action="" method="post">
-                    <input type="hidden" name="edit_id" value="">
-                    <button  type="submit" name="edit_btn" class="btn btn-success"> EDIT</button>
+                  <input type="hidden" name="edit_id" value="">
+                  <button type="submit" name="edit_btn" class="btn btn-success"> EDIT</button>
                 </form>
-            </td>
-            <td>
+              </td>
+              <td>
                 <form action="" method="post">
                   <input type="hidden" name="delete_id" value="">
                   <button type="submit" name="delete_btn" class="btn btn-danger"> DELETE</button>
                 </form>
-            </td>
-          </tr>
-        
-        </tbody>
-      </table>
+              </td>
+            </tr>
 
+          </tbody>
+        </table>
+
+      </div>
     </div>
   </div>
-</div>
 
 </div>
 <!-- /.container-fluid -->
