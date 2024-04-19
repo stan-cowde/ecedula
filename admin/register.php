5c20b6ae8cfad6ac96ee06eb9157b38998f564ce
Estanley Tagalog <estanleyjt@gmail.com>
2024-04-19 23:12:16 +0800

    updated

--- admin/register.php
+++ admin/register.php
@@ -89,48 +89,7 @@ include "includes/header.php";
         </div>
       </div>
 
-  <div class="card-body">
-
-    <div class="table-responsive">
-
-      <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
-        <thead>
-          <tr>
-            <th> ID </th>
-            <th> Username </th>
-            <th>Email </th>
-            <th>Password</th>
-            <th>EDIT </th>
-            <th>DELETE </th>
-          </tr>
-        </thead>
-        <tbody>
-     
-          <tr>
-            <td> 1 </td>
-            <td> peter</td>
-            <td> peter@gmail.com</td>
-            <td> *** </td>
-            <td>
-                <form action="" method="post">
-                    <input type="hidden" name="edit_id" value="">
-                    <button  type="submit" name="edit_btn" class="btn btn-success"> Approved</button>
-                </form>
-            </td>
-            <td>
-                <form action="" method="post">
-                  <input type="hidden" name="delete_id" value="">
-                  <button type="submit" name="delete_btn" class="btn btn-danger"> Disapproved</button>
-                </form>
-            </td>
-          </tr>
-        
-        </tbody>
-      </table>
-
-    </div>
-
-  </div>
+  
 
 </div>
 
