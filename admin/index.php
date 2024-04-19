6b5bc7c377e62f59e78ca27f65d57f916e8a81c5
Estanley Tagalog <estanleyjt@gmail.com>
2024-04-19 23:19:09 +0800

    index.php

--- admin/index.php
+++ admin/index.php
@@ -14,91 +14,54 @@ include('includes/navbar.php');
         class="fas fa-download fa-sm text-white-50"></i> Generate Report</a>
   </div>
 
-  <!-- Content Row -->
-  <div class="row">
+  <div class="card-body">
 
-    <!-- Earnings (Monthly) Card Example -->
-    <div class="col-xl-3 col-md-6 mb-4">
-      <div class="card border-left-primary shadow h-100 py-2">
-        <div class="card-body">
-          <div class="row no-gutters align-items-center">
-            <div class="col mr-2">
-              <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Total Registered </div>
-              <div class="h5 mb-0 font-weight-bold text-gray-800">
+    <div class="table-responsive">
 
-               <h4>Total Admin: 1</h4>
+      <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+        <thead>
+          <tr>
+            <th> ID </th>
+            <th> Username </th>
+            <th>Email </th>
+            <th>Password</th>
+            <th>EDIT </th>
+            <th>DELETE </th>
+          </tr>
+        </thead>
+        <tbody>
+     
+          <tr>
+            <td> 1 </td>
+            <td> peter</td>
+            <td> peter@gmail.com</td>
+            <td> *** </td>
+            <td>
+                <form action="" method="post">
+                    <input type="hidden" name="edit_id" value="">
+                    <button  type="submit" name="edit_btn" class="btn btn-success"> Approved</button>
+                </form>
+            </td>
+            <td>
+                <form action="" method="post">
+                  <input type="hidden" name="delete_id" value="">
+                  <button type="submit" name="delete_btn" class="btn btn-danger"> Disapproved</button>
+                </form>
+            </td>
+          </tr>
+        
+        </tbody>
+      </table>
 
-              </div>
-            </div>
-            <div class="col-auto">
-              <i class="fas fa-calendar fa-2x text-gray-300"></i>
-            </div>
-          </div>
-        </div>
-      </div>
     </div>
 
-    <!-- Earnings (Monthly) Card Example -->
-<!--    <div class="col-xl-3 col-md-6 mb-4">
-      <div class="card border-left-success shadow h-100 py-2">
-        <div class="card-body">
-          <div class="row no-gutters align-items-center">
-            <div class="col mr-2">
-              <div class="text-xs font-weight-bold text-success text-uppercase mb-1">Earnings (Annual)</div>
-              <div class="h5 mb-0 font-weight-bold text-gray-800">$215,000</div>
-            </div>
-            <div class="col-auto">
-              <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
--->
+  </div>
+  
 
     <!-- Earnings (Monthly) Card Example -->
- <!--   <div class="col-xl-3 col-md-6 mb-4">
-      <div class="card border-left-info shadow h-100 py-2">
-        <div class="card-body">
-          <div class="row no-gutters align-items-center">
-            <div class="col mr-2">
-              <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Tasks</div>
-              <div class="row no-gutters align-items-center">
-                <div class="col-auto">
-                  <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">50%</div>
-                </div>
-                <div class="col">
-                  <div class="progress progress-sm mr-2">
-                    <div class="progress-bar bg-info" role="progressbar" style="width: 50%" aria-valuenow="50"
-                      aria-valuemin="0" aria-valuemax="100"></div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="col-auto">
-              <i class="fas fa-clipboard-list fa-2x text-gray-300"></i>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
--->
+ 
     <!-- Pending Requests Card Example -->
-    <div class="col-xl-3 col-md-6 mb-4">
-      <div class="card border-left-warning shadow h-100 py-2">
-        <div class="card-body">
-          <div class="row no-gutters align-items-center">
-            <div class="col mr-2">
-              <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">Pending Requests</div>
-              <div class="h5 mb-0 font-weight-bold text-gray-800">1</div>
-            </div>
-            <div class="col-auto">
-              <i class="fas fa-comments fa-2x text-gray-300"></i>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+  
   </div>
 
   <!-- Content Row -->
