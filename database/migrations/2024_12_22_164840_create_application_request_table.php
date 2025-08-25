bc15980cfb39c9cb7286c5c75587af750dd6f52f
Venz Olarte <venzolarte@gmail.com>
2025-08-26 01:38:34 +0800

    new migration

---
+++ database/migrations/2024_12_22_164840_create_application_request_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('application_request', function (Blueprint $table) {
+            $table->integer('id', true);
+            $table->integer('user_id')->nullable();
+            $table->enum('status', ['Pending', 'Approved', 'Denied'])->nullable()->default('Pending');
+            $table->integer('reviewed_by')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('application_request');
+    }
+};
---
+++ database/migrations/2024_12_22_164840_create_transactions_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->integer('id', true);
+            $table->integer('user_id')->nullable();
+            $table->string('transaction_code')->nullable();
+            $table->decimal('amount', 20)->nullable();
+            $table->enum('status', ['PENDING', 'APPROVED'])->default('PENDING');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
